71a081f4235955d638380c7957268c4b72f56d78
ZorEl212 <yabsirad212@gmail.com>
2025-11-28 18:40:20 +0000

    properties: Add default props for PropsHooksUtils
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -30,6 +30,29 @@ PRODUCT_SYSTEM_PROPERTIES += \
 PRODUCT_PACKAGES += \
     GoogleCameraGo
 
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_mainline_BRAND?=google \
+    persist.sys.pihooks_mainline_DEVICE?=caiman \
+    persist.sys.pihooks_mainline_MANUFACTURER?=Google \
+    persist.sys.pihooks_mainline_PRODUCT?=caiman \
+    ro.product.model_for_attestation?=Pixel 9 Pro \
+    ro.product.brand_for_attestation?=google \
+    ro.product.name_for_attestation?=caiman \
+    ro.product.device_for_attestation?=caiman \
+    ro.product.manufacturer_for_attestation?=Google \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_DEVICE?=oriole \
+    persist.sys.pihooks_PRODUCT?=oriole_beta \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SECURITY_PATCH?=2024-12-05 \
+    persist.sys.pihooks_ID?=BP21.241121.009
+
+PIHOOKS_BUILD_GMS_FINGERPRINT := google/oriole_beta/oriole:Baklava/BP21.241121.009/12787338:user/release-keys
+PIHOOKS_BUILD_FINGERPRINT := google/caiman/caiman:14/AD1A.240530.047.U1/12150698:user/release-keys
+PIHOOKS_MODEL_SPOOF := "Pixel 9 Pro"
+
 # GameSpace
 PRODUCT_PACKAGES += \
      GameSpace
